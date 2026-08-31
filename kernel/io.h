479bbf26523fada24de44f0c7988ddc49cd6e393
Skokoo <nourriwilliam789@gmail.com>
2026-08-31 17:12:45 +0700

    Update io.h

--- kernel/io.h
+++ kernel/io.h
@@ -3,49 +3,34 @@
 
 static inline unsigned char inb(unsigned short p) {
     unsigned char r;
-    __asm__ volatile (
-        "inb %1, %0" 
-        : "=a"(r) 
-        : "Nd"(p) 
-        : "memory"
-    );
+    __asm__ volatile ("inb %1, %0" : "=a"(r) : "Nd"(p) : "memory");
     return r;
 }
 
 static inline void outb(unsigned short p, unsigned char v) {
-    __asm__ volatile (
-        "outb %0, %1" 
-        : 
-        : "a"(v), "Nd"(p) 
-        : "memory"
-    );
+    __asm__ volatile ("outb %0, %1" :: "a"(v), "Nd"(p) : "memory");
 }
 
 static inline void io_wait(void) {
+    __asm__ volatile ("outb %%al, $0x80" :: "a"(0) : "memory");
+}
+
+static inline void mcpy64(void* dest, const void* src, unsigned int count) {
     __asm__ volatile (
-        "outb %%al, $0x80" 
-        : 
-        : "a"(0) 
+        "cld\n\t"
+        "rep movsq"
+        : "+D"(dest), "+S"(src), "+c"(count)
+        :
         : "memory"
     );
 }
 
-static inline void insw(unsigned short p, void* addr, unsigned int count) {
+static inline void mset64(void* dest, unsigned long long val, unsigned int count) {
     __asm__ volatile (
         "cld\n\t"
-        "rep insw" 
-        : "+D"(addr), "+c"(count) 
-        : "d"(p) 
-        : "memory"
-    );
-}
-
-static inline void outsw(unsigned short p, const void* addr, unsigned int count) {
-    __asm__ volatile (
-        "cld\n\t"
-        "rep outsw" 
-        : "+S"(addr), "+c"(count) 
-        : "d"(p) 
+        "rep stosq"
+        : "+D"(dest), "+c"(count)
+        : "a"(val)
         : "memory"
     );
 }
