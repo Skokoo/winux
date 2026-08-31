e653a4994f236ba0ecb07a2bbd2fb32dda8c75ab
Skokoo <nourriwilliam789@gmail.com>
2026-08-31 17:28:23 +0700

    Update vga.h

--- kernel/vga.h
+++ kernel/vga.h
@@ -14,12 +14,30 @@ void mv(int pos) {
     outb(0x3D4, 0x0E); outb(0x3D5, (unsigned char)((pos >> 8) & 0xFF));
 }
 
+void scroll(void) {
+    void* dest = (void*)VGA;
+    const void* src = (const void*)(VGA + (80 * 2));
+    mcpy64(dest, src, (24 * 80 * 2) / 8);
+    
+    void* clear_line = (void*)(VGA + (24 * 80 * 2));
+    unsigned long long empty_val = 0x0720072007200720ULL;
+    mset64(clear_line, empty_val, (80 * 2) / 8);
+    
+    p = 24 * 80;
+}
+
 void pr(const char* s) {
     volatile unsigned short* b = (volatile unsigned short*)VGA;
     unsigned short attr = COL << 8;
     int cur = p;
+
     while (*s) {
-        if ((unsigned int)cur >= 2000) cur = 0;
+        if ((unsigned int)cur >= 2000) {
+            p = cur;
+            scroll();
+            cur = p;
+            b = (volatile unsigned short*)VGA;
+        }
         b[cur++] = attr | (unsigned char)*s++;
     }
     p = cur;
@@ -28,8 +46,19 @@ void pr(const char* s) {
 
 void newline(void) {
     p = ((p / 80) + 1) * 80;
-    if (p >= 2000) p = 0;
+    if (p >= 2000) {
+        scroll();
+    }
     mv(p);
+}
+
+void vga_backspace(void) {
+    if (p > 0) {
+        p--;
+        volatile unsigned short* b = (volatile unsigned short*)VGA;
+        b[p] = 0x0700;
+        mv(p);
+    }
 }
 
 #endif
